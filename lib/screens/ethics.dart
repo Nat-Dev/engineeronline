0d099ebccd43320292e941bb1f98274d94a930d9
Nat-Dev <nat.sherlockianz@gmail.com>
2021-03-08 10:45:47 +0700

    change appbar for all page except home

--- lib/screens/ethics.dart
+++ lib/screens/ethics.dart
@@ -26,57 +26,25 @@ class _EthicsState extends State<Ethics> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(70),
-        child: AppBar(
-          centerTitle: true,
-          leading: backButton(),
-          title: Text(
-            "วิศวกร EIT Online",
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.yellowAccent,
-                shadows: [
-                  Shadow(
-                    offset: Offset(1.75, 1.75),
-                    blurRadius: 3.0,
-                    color: Color.fromARGB(255, 0, 0, 0),
-                  )
-                ]),
+      appBar: AppBar(
+        centerTitle: true,
+        leading: backButton(),
+        backgroundColor: Colors.blue.shade900,
+        title: Text(
+          "จรรยาบรรณ",
+          style: TextStyle(
+            color: Colors.yellowAccent,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            shadows: [
+              Shadow(
+                offset: Offset(1.75, 1.75),
+                blurRadius: 3.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              )
+            ],
           ),
-          backgroundColor: Colors.red.shade800,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-            bottomRight: Radius.circular(50),
-            bottomLeft: Radius.circular(50),
-          )),
         ),
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 8,
-          ),
-          AppBar(
-            centerTitle: true,
-            backgroundColor: Colors.blue.shade900,
-            title: Text(
-              "จรรยาบรรณ",
-              style: TextStyle(
-                  color: Colors.yellowAccent,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  shadows: [
-                    Shadow(
-                      offset: Offset(1.75, 1.75),
-                      blurRadius: 3.0,
-                      color: Color.fromARGB(255, 0, 0, 0),
-                    )
-                  ]),
-            ),
-          )
-        ],
       ),
     );
   }
--- lib/screens/general.dart
+++ lib/screens/general.dart
@@ -26,57 +26,26 @@ class _GeneralState extends State<General> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(70),
-          child: AppBar(
-            leading: backButton(),
-            centerTitle: true,
-            title: Text(
-              "วิศวกร EIT Online",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.yellowAccent,
-                  shadows: [
-                    Shadow(
-                      offset: Offset(1.75, 1.75),
-                      blurRadius: 3.0,
-                      color: Color.fromARGB(255, 0, 0, 0),
-                    )
-                  ]),
-            ),
-            backgroundColor: Colors.red.shade800,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(50),
-              bottomLeft: Radius.circular(50),
-            )),
+      appBar: AppBar(
+        centerTitle: true,
+        leading: backButton(),
+        backgroundColor: Colors.blue.shade900,
+        title: Text(
+          "ความรู้วิศวกรรมในงานก่อสร้าง",
+          style: TextStyle(
+            color: Colors.yellowAccent,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            shadows: [
+              Shadow(
+                offset: Offset(1.75, 1.75),
+                blurRadius: 3.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              )
+            ],
           ),
         ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 8,
-            ),
-            AppBar(
-              centerTitle: true,
-              backgroundColor: Colors.blue.shade900,
-              title: Text(
-                "ความรู้วิศวกรรมในงานก่อสร้าง",
-                style: TextStyle(
-                    color: Colors.yellowAccent,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(1.75, 1.75),
-                        blurRadius: 3.0,
-                        color: Color.fromARGB(255, 0, 0, 0),
-                      )
-                    ]),
-              ),
-            )
-          ],
-        ));
+      ),
+    );
   }
 }
