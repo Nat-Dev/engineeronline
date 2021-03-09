667f2f5bbff95e842a0cff3b96e3e6e4b03c33c0
Nat-Dev <nat.sherlockianz@gmail.com>
2021-03-09 10:49:39 +0700

    webview page show website with model's url

--- lib/screens/webview.dart
+++ lib/screens/webview.dart
@@ -1,5 +1,6 @@
 import 'package:engineeronline/models/general_model.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Webview extends StatefulWidget {
   final GeneralModel generalModel;
@@ -21,8 +22,29 @@ class _WebviewState extends State<Webview> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(model.name == null ? "Open Website" : model.name),
+        backgroundColor: Colors.blue.shade900,
+        title: Text(
+          model.name == null ? "Open Website" : model.name,
+          style: TextStyle(
+            color: Colors.yellowAccent,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            shadows: [
+              Shadow(
+                offset: Offset(1.75, 1.75),
+                blurRadius: 3.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              )
+            ],
+          ),
+        ),
       ),
+      body: model.url == null
+          ? Center(child: CircularProgressIndicator())
+          : WebView(
+              initialUrl: model.url,
+              javascriptMode: JavascriptMode.unrestricted,
+            ),
     );
   }
 }
