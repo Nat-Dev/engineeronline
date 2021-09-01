a819264e0586c31663aa02973748ff3b1f2d3b88
Nat-Dev <nat.sherlockianz@gmail.com>
2021-09-01 14:16:53 +0700

    structure_work post both web and youtube

--- lib/screens/contents/general/structure_work.dart
+++ lib/screens/contents/general/structure_work.dart
@@ -1,7 +1,10 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:engineeronline/models/website_model.dart';
+import 'package:engineeronline/models/youtube_model.dart';
 import 'package:engineeronline/screens/posts/web_post.dart';
+import 'package:engineeronline/screens/posts/youtube_post.dart';
 import 'package:engineeronline/screens/views/website.dart';
+import 'package:engineeronline/screens/views/youtube.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -14,7 +17,7 @@ class StructureWork extends StatefulWidget {
 
 class _StructureWorkState extends State<StructureWork> {
   List<Widget> widgets = [];
-  List<WebsiteModel> generalModels = [];
+  List<dynamic> generalModels = [];
 
   @override
   void initState() {
@@ -33,18 +36,28 @@ class _StructureWorkState extends State<StructureWork> {
         int index = 0;
         for (var snapshot in event.docs) {
           Map<String, dynamic> map = snapshot.data();
-          WebsiteModel model = WebsiteModel.fromMap(map);
-          generalModels.add(model);
-          setState(() {
-            widgets.add(createWidget(model, index));
-          });
+          if (WebsiteModel.fromMap(map).type == "yt") {
+            // change to youtube
+            YoutubeModel model = YoutubeModel.fromMap(map);
+            generalModels.add(model);
+            setState(() {
+              widgets.add(createYoutubeWidget(model, index));
+            });
+          } else {
+            // change to web
+            WebsiteModel model = WebsiteModel.fromMap(map);
+            generalModels.add(model);
+            setState(() {
+              widgets.add(createWebWidget(model, index));
+            });
+          }
           index++;
         }
       });
     });
   }
 
-  Widget createWidget(WebsiteModel model, int index) => GestureDetector(
+  Widget createWebWidget(WebsiteModel model, int index) => GestureDetector(
         onTap: () {
           Navigator.push(
               context,
@@ -54,7 +67,6 @@ class _StructureWorkState extends State<StructureWork> {
               ));
         },
         child: Card(
-          color: Colors.amber.shade100,
           elevation: 5,
           child: Center(
             child: Column(
@@ -69,6 +81,50 @@ class _StructureWorkState extends State<StructureWork> {
                 AutoSizeText(
                   'โดย ' + model.username,
                   maxLines: 1,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue.shade900,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+  Widget createYoutubeWidget(YoutubeModel model, int index) => GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    Youtube(youtubeModel: generalModels[index]),
+              ));
+        },
+        child: Card(
+          elevation: 5,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  width: 180,
+                  child: Image.network(model.thumbnail),
+                ),
+                Flexible(
+                  child: AutoSizeText(
+                    model.name,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                AutoSizeText(
+                  model.username,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 14,
                     fontWeight: FontWeight.bold,
@@ -129,6 +185,62 @@ class _StructureWorkState extends State<StructureWork> {
     );
   }
 
+  void postAlert() {
+    showDialog(
+      barrierDismissible: true,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: ListTile(
+            leading: Icon(
+              Icons.add_box_rounded,
+              color: Colors.green,
+              size: 48.0,
+            ),
+            title: Text(
+              "กรุณาเลือกรูปแบบของหัวข้อ",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          content: Text("เพิ่มหัวข้อใหม่แบบ Website อ้างอิงหรือลิ้งค์ YouTube"),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Close"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: Text("Website"),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            WebPost("general_structure_work")));
+              },
+            ),
+            TextButton(
+              child: Text("YouTube"),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            YoutubePost("general_structure_work")));
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -158,14 +270,8 @@ class _StructureWorkState extends State<StructureWork> {
                 FirebaseAuth firebaseAuth = FirebaseAuth.instance;
                 User user = firebaseAuth.currentUser;
                 if (user != null) {
-                  print("logged in");
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              WebPost("general_structure_work")));
+                  postAlert();
                 } else {
-                  print("please sign in first");
                   authenAlert();
                 }
               })
@@ -174,7 +280,7 @@ class _StructureWorkState extends State<StructureWork> {
       body: widgets.length == 0
           ? Center(child: CircularProgressIndicator())
           : Container(
-              decoration: BoxDecoration(color: Colors.grey.shade200),
+              decoration: BoxDecoration(color: Colors.yellow.shade100),
               child: ListView(
                 children: widgets,
               ),
