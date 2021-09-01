36e56dfd07693b2319f63e93f817c7133a5a3bee
Nat-Dev <nat.sherlockianz@gmail.com>
2021-09-01 15:05:23 +0700

    precast_work post both web and youtube

--- lib/screens/contents/technique/precast_work.dart
+++ lib/screens/contents/technique/precast_work.dart
@@ -1,5 +1,8 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:engineeronline/models/website_model.dart';
+import 'package:engineeronline/screens/posts/web_post.dart';
 import 'package:engineeronline/screens/posts/youtube_post.dart';
+import 'package:engineeronline/screens/views/website.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -14,7 +17,7 @@ class PrecastWork extends StatefulWidget {
 
 class _PrecastWorkState extends State<PrecastWork> {
   List<Widget> widgets = [];
-  List<YoutubeModel> techniqueModels = [];
+  List<dynamic> techniqueModels = [];
 
   @override
   void initState() {
@@ -33,18 +36,64 @@ class _PrecastWorkState extends State<PrecastWork> {
         int index = 0;
         for (var snapshot in event.docs) {
           Map<String, dynamic> map = snapshot.data();
-          YoutubeModel model = YoutubeModel.fromMap(map);
-          techniqueModels.add(model);
-          setState(() {
-            widgets.add(createWidget(model, index));
-          });
+          if (WebsiteModel.fromMap(map).type == "yt") {
+            // change to youtube
+            YoutubeModel model = YoutubeModel.fromMap(map);
+            techniqueModels.add(model);
+            setState(() {
+              widgets.add(createYoutubeWidget(model, index));
+            });
+          } else {
+            // change to web
+            WebsiteModel model = WebsiteModel.fromMap(map);
+            techniqueModels.add(model);
+            setState(() {
+              widgets.add(createWebWidget(model, index));
+            });
+          }
           index++;
         }
       });
     });
   }
 
-  Widget createWidget(YoutubeModel model, int index) => GestureDetector(
+  Widget createWebWidget(WebsiteModel model, int index) => GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    Website(websiteModel: techniqueModels[index]),
+              ));
+        },
+        child: Card(
+          elevation: 5,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                AutoSizeText(
+                  model.name,
+                  textAlign: TextAlign.center,
+                  maxLines: 2,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                AutoSizeText(
+                  'โดย ' + model.username,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue.shade900,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+  Widget createYoutubeWidget(YoutubeModel model, int index) => GestureDetector(
         onTap: () {
           Navigator.push(
               context,
@@ -136,6 +185,62 @@ class _PrecastWorkState extends State<PrecastWork> {
     );
   }
 
+  void postAlert() {
+    showDialog(
+      barrierDismissible: true,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: ListTile(
+            leading: Icon(
+              Icons.add_box_rounded,
+              color: Colors.green,
+              size: 48.0,
+            ),
+            title: Text(
+              "กรุณาเลือกรูปแบบของหัวข้อ",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          content: Text("เพิ่มหัวข้อใหม่แบบ Website อ้างอิงหรือลิ้งค์ YouTube"),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Close"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: Text("Website"),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            WebPost("technique_precast_work")));
+              },
+            ),
+            TextButton(
+              child: Text("YouTube"),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            YoutubePost("technique_precast_work")));
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -164,14 +269,8 @@ class _PrecastWorkState extends State<PrecastWork> {
                 FirebaseAuth firebaseAuth = FirebaseAuth.instance;
                 User user = firebaseAuth.currentUser;
                 if (user != null) {
-                  print("logged in");
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              YoutubePost("technique_precast_work")));
+                  postAlert();
                 } else {
-                  print("please sign in first");
                   authenAlert();
                 }
               })
@@ -180,7 +279,7 @@ class _PrecastWorkState extends State<PrecastWork> {
       body: widgets.length == 0
           ? Center(child: CircularProgressIndicator())
           : Container(
-              decoration: BoxDecoration(color: Colors.grey.shade200),
+              decoration: BoxDecoration(color: Colors.yellow.shade100),
               child: ListView(
                 children: widgets,
               ),
